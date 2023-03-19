caf3c6bdd1a7a2e09f369f4b3787042e1254edaf
charles smith <char_smith@hotmail.com>
2023-03-19 13:22:57 -0400

    modiifcation caroussel ajoutant un ID

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -16,115 +16,57 @@ nav();
 
 });
 
+let sliderImages = document.querySelectorAll(".active"),
+    btnPrev = document.getElementById('btn-prev'),
+    btnRight = document.getElementById('btn-right'),
+    current = 0;
 
-let currentImage = 0;
-let elements, slides , slideWidth, showImage, speed;
+    //console.log(sliderImages);
 
-window.onload =()=>{
-  const image = document.querySelector ('full-slide')
- /**  speed = image.dataset.speed;
-
-
-  /**image.forEach((active, indx)=>{
-    active.style.transform = 'translateX(${indx * 100}%)';
-  });**/
-
-  elements = document.querySelector('.banner')
-
-
-  console.log(elements);
-
-  let firstImage = elements.firstElementChild.cloneNode(true);
-
-
-slides = Array.from(elements.children)
-console.log(slides);
-
-/**slideWidth = image.getBoundingClientRect().width;**/
-
-
-  let prev = document.querySelector(".icon-chevron-thin-left prev");
-  let next = document.querySelector(".icon-chevron-thin-left next");
-
-  prev = addEventListener('click', slidePrev)
-  next = addEventListener('click', slidenext)
-
-  console.log(prev);
-
-  //automatiser 
-  timer = setInterval (slidenext,speed);
-
-  window.addEventListener("DOMContentLoaded", showImage);
-
-  console.log(timer);
-}
-
-function slidePrev() {
-  currentImage --;
-  elements.style.transition ='is linear';
-  if (currentImage < 0 ) {
-    currentImage = slides.length-1;
-    let decal = -slideWidth * currentImage;
-    elements.style.transition = 'unset';
-    elements.style.transform = 'translateX(0)';
-
-  }
-  /**showImage(currentImage);**/
-};
-console.log(currentImage);
-
-function slidenext(){
-  currentImage ++;
-
-    if (currentImage > slides.length - 1) {
-      currentImage = 0;
+    // Nettoyage des images
+    function reset() {
+    for (let i = 0; i < sliderImages.length; i++) {
+    	sliderImages[i].style.display = "none";
+        }
     }
-   /**  showImage(currentImage);**/
-  };
-;
+    
+    // Démarrage 
+    function startSlide() {
+    reset();
+    sliderImages[0].style.display = "block";
+    }
+    
+    // Montrer photo vers la gauche
+    function slidePrev() {
+    reset();
+    sliderImages[current - 1].style.display = "block";
+    current--;
+    }
+    
+    // montrer photo vers la droite
+    function slideRight() {
+    reset();
+    sliderImages[current + 1].style.display = "block";
+    current++;
+    }
+    
+    // bouton previous
+    btnPrev.addEventListener("click", function () {
+    if (current === 0) {
+    	current = sliderImages.length;
+    }
+    slideLeft();
+    });
+    
+    // bouton right 
+    btnRight.addEventListener("click", function () {
+    if (current === sliderImages.length - 1) {
+    	current = -1;
+    }
+    slideRight();
+    });
+    
+    startSlide();
 
 
-
-/** 
-
-// the variables of the image slider
-const image = document.querySelector(".active");
-const prevBtn = document.querySelector(".icon-chevron-thin-left prev");
-const nextBtn = document.querySelector(".icon-chevron-thin-right next");
-
-// the image array
-const images = [
-	"../img/full-slide/thumb-1.jpg","../img/full-slide/thumb-2.jpg","../img/full-slide/thumb-1.jpg",
-];
-
-// the index of the image on page load
-let currentImage = 0;
-
-// the image details that shows when the webpage loads
-window.addEventListener("DOMContentLoaded", showImage);
-
-// function to select and change the image details
-function showImage() {
-  image.src = images[currentImage];
-}
-
-
-// the next button function
-nextBtn.addEventListener("click", function () {
-  currentImage++;
-  if (currentImage > images.length - 1) {
-    currentImage = 0;
-  }
-  showImage(currentImage);
-});
-
-// the prev button function
-prevBtn.addEventListener("click", function () {
-  currentImage--;
-  if (currentImage < 0) {
-    currentImage = images.length - 1;
-  }
-  showImage(currentImage);
-});
-
-**/
+    
